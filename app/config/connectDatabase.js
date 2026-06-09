5174cf06179af8fd771b812dc571120a6410ec37
sathishkumardevelopments <itsmymailusage@gmail.com>
2026-06-10 00:40:57 +0530

    Update connectDatabase.js

--- app/config/connectDatabase.js
+++ app/config/connectDatabase.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const connectDatabase = async() => {
     try{
-        await mongoose.connect(process.env.DATABASE_URL).then(() => {
+        await mongoose.connect(process.env.MONGO_URI).then(() => {
             console.log('OK')
         })
     }catch(err){
